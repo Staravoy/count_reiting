9162414f315844319c6efdeb3c90cb41fa95e191
Staravoy <85824511+Staravoy@users.noreply.github.com>
2023-07-31 11:47:17 +0300

    Завершена програма
    Додано діалогове вікно
    Додано цикл while

--- rating_for_PSPOP.py
+++ rating_for_PSPOP.py
@@ -1,11 +1,22 @@
 import openpyxl
 from generate_name_colum import generate_name_column
+import tkinter as tk
+from tkinter import filedialog
 
+
+def dialog_window():
+    # Створення головного вікна
+    root = tk.Tk()
+    root.withdraw()
+    # Відкриваємо діалогове вікно для вибору файлу
+    file_path = filedialog.askopenfilename(title="Оберіть файл для обробки", filetypes=[("All Files", "*.*")])
+    return file_path
 
 def load_file_and_all_sheets():
-    file = 'rozstanovka.xlsx'
-    wb = openpyxl.load_workbook(file, data_only=True)
+    file_path = dialog_window()
+    wb = openpyxl.load_workbook(file_path, data_only=True)
     list_all_sheets = wb.sheetnames
+    wb.close()
     return wb, list_all_sheets
 
 
@@ -30,8 +41,7 @@ def get_letter_work_column(messange):
             print("Ви ввели не правильний символ, спробуйте ще раз!")
 
 
-def get_all_names_force(star_row, end_row):
-    work_file, work_sheets = load_file_and_all_sheets()
+def get_all_names_force(star_row, end_row, work_file, work_sheets):
     sheet = work_file[work_sheets[0]]
     names_force = []
     for row in sheet.iter_rows(min_row=int(star_row), max_row=int(end_row), min_col=1, max_col=1):
@@ -59,43 +69,54 @@ def fill_table(name_file, num_row, my_list):
         sheet.cell(row=num_row, column=col, value=x)
         col += 1
     wb.save(name)
+    wb.close()
+
+while True:
+    # отримання даних необхідних для роботи
+    print("Слава Україні!!!")
+    print("Вас вітає програма: ПІДРАХУЙКА!!!")
+    print("її завдання полегшити процес збирання інформації.")
+    input("Ну, що до роботи? Натисніть ENTER")
+    print("Зчитуємо файл...")
+    # завантажили робочу книгу з усіма листами
+    file, sheets = load_file_and_all_sheets()
+    start = get_star_and_end_row("Введіть номер рядку ПЕРШОГО підрозділу в списку:")
+    end = get_star_and_end_row("Введіть номер рядку ОСТАННЬОГО підрозділу в списку:")
+    work_column = get_letter_work_column("Введіть літеру або літери, колонки яку треба опрацювати:")
+    # отримуємо назву нового документу та створюємо його
+    name_new_table = create_new_excel_table()
+    print("Збір даних розпочато...")
+
+    # отримуємо імена всіх підрозділів
+    name_forces = get_all_names_force(start, end, file, sheets)
+
+    # створюємо шапку таблиці з обрізаних назв листів
+    list_sheet_name = [" "]
+    for sheet in sheets:
+        list_sheet_name.append(sheet[:-5])
+    fill_table(name_new_table, 1, list_sheet_name)
 
 
-# отримання даних необхідних для роботи
-start = get_star_and_end_row("Введіть номер рядку ПЕРШОГО підрозділу в списку:")
-end = get_star_and_end_row("Введіть номер рядку ОСТАННЬОГО підрозділу в списку:")
-work_column = get_letter_work_column("Введіть літеру або літери, колонки яку треба опрацювати:")
-# отримуємо назву нового документу та створюємо його
-name_new_table = create_new_excel_table()
-print("Збір даних розпочато...")
+    num_row = 2
+    for name in range(0, len(name_forces)):
+        list_for_new_table = []
+        list_for_new_table.append(name_forces[name])
+        for one_sheet in sheets:
+            sheet = file[one_sheet]
+            # шукаємо номер рядку підрозділу на кожному аркуші
+            for i in range(int(start), int(end) + 1):
+                cell_value = sheet.cell(row=i, column=1).value
+                if cell_value == name_forces[name]:
+                    row_one_force = i
+            cell = sheet.cell(row=row_one_force, column=work_column).value
+            if not cell:
+                continue
+            list_for_new_table.append(cell)
+        fill_table(name_new_table, num_row, list_for_new_table)
+        num_row += 1
+    print("Збір даних завершено!")
+    question = input("Чи бажаєте продовжити роботу? (так/ні)")
+    if question == 'ні':
+        break
 
-# отримуємо імена всіх підрозділів
-name_forces = get_all_names_force(start, end)
-# завантажили робочу книгу з усіма листами
-file, sheets = load_file_and_all_sheets()
-# створюємо шапку таблиці з обрізаних назв листів
-list_sheet_name = [" "]
-for sheet in sheets:
-    list_sheet_name.append(sheet[:-5])
-fill_table(name_new_table, 1, list_sheet_name)
-
-
-num_row = 2
-for name in range(0, len(name_forces)):
-    list_for_new_table = []
-    list_for_new_table.append(name_forces[name])
-    for one_sheet in sheets:
-        sheet = file[one_sheet]
-        # шукаємо номер рядку підрозділу на кожному аркуші
-        for i in range(int(start), int(end) + 1):
-            cell_value = sheet.cell(row=i, column=1).value
-            if cell_value == name_forces[name]:
-                row_one_force = i
-        cell = sheet.cell(row=row_one_force, column=work_column).value
-        if not cell:
-            continue
-        list_for_new_table.append(cell)
-    fill_table(name_new_table, num_row, list_for_new_table)
-    num_row += 1
-print("Збір даних завершено!")
 
--- test.py
+++ test.py
@@ -1,32 +1,12 @@
-import openpyxl
+import tkinter as tk
+from tkinter import filedialog
 
-file = 'rozstanovka.xlsx'
-wb = openpyxl.load_workbook(file, data_only=True)
-all_sheets = wb.sheetnames
-name_sheets_for_table = []
-for x in all_sheets:
-    name_sheets_for_table.append(x[:-5])
-name_sheets_for_table = [' '] + name_sheets_for_table
+# Створення головного вікна
+root = tk.Tk()
+root.withdraw()
 
-new_table = openpyxl.Workbook()
-new_table.save('new_table.xlsx')
+# Відкриваємо діалогове вікно для вибору файлу
+file_path = filedialog.askopenfilename(title="Оберіть файл для обробки", filetypes=[("All Files", "*.*")])
 
-def fill_table(num_row, my_list):
-    # Створюємо новий документ Excel
-    wb = openpyxl.load_workbook('new_table.xlsx')
-    # Вибираємо активний аркуш
-    sheet = wb.active
-    col = 1
-    for x in my_list:
-        sheet.cell(row=num_row, column=col, value=x)
-        col += 1
-    wb.save('new_table.xlsx')
-
-fill_table(1, name_sheets_for_table)
-
-second_row = ['value1', 'value2', 'value3', 'value4', 'value5', 'value6', 'value7']
-
-therd_row = ['value1', 'value2', 'value3', 'value4', 'value5', 'value6', 'value7']
-
-fill_table(2, second_row)
-fill_table(3, therd_row)
+# Виведення шляху до вибраного файлу
+print("Обраний файл:", file_path)
